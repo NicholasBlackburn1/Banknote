a53010759abf4e968cded9415db2207ae542980f
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-01 22:27:04 -0500

    More tests

--- spec/banknote_spec.rb
+++ spec/banknote_spec.rb
@@ -1,4 +1,12 @@
 RSpec.describe Banknote do
+
+  it "checks timecodes" do
+    expect(Cache.write("DATA")).to be 0
+    puts("Testing timecodes...")
+    sleep(2)
+    expect(Cache.stale(1)).to be true
+  end
+
   it "has a version number" do
     expect(Banknote::VERSION).not_to be nil
   end
@@ -11,14 +19,8 @@ RSpec.describe Banknote do
     expect(Cache.write("Cache file written.")).to be 0
   end
 
-  it "checks timecodes" do
-    expect(Cache.write("DATA")).to be 0
-    sleep(2)
-    expect(Cache.stale(1)).to be true
+  it "reads a cache file" do
+    expect(Cache.write("READ")).to be 0
+    expect(Cache.read().slice(/READ/)).to eq("READ")
   end
-
-  # it "reads a cache file" do
-  #   expect(Cache.write("READ")).to be 0
-  #   expect(Cache.read()).to eq("READ")
-  # end
 end
